a463d3e046f4e9db4b171ff7061719bc06e6e898
Manuel Guilherme <60741154+Galmanus@users.noreply.github.com>
2024-01-20 17:18:29 -0300

    Update saidasRoutes.js

--- routes/saidasRoutes.js
+++ routes/saidasRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const SaidaController = require('../controllers/SaidaController');
 
 // Rota para criar uma nova saida
-router.post('/Saidas', saidaController.createSaida);
+router.post('/saidas', saidaController.createSaida);
 
 // Rota para obter todos as saidas
-router.get('/Saidas', saidaController.getAllSaidas);
+router.get('/saidas', saidaController.getAllSaidas);
 
 // Rota para obter uma saida pelo ID
-router.get('/Saidas/:id', saidaController.getSaidaById);
+router.get('/saidas/:id', saidaController.getSaidaById);
 
 // Rota para atualizar uma saida
-router.put('/Saidas/:id', saidaController.updateSaida);
+router.put('/saidas/:id', saidaController.updateSaida);
 
 // Rota para deletar uma saida
-router.delete('/Saidas/:id', saidaController.deleteSaida);
+router.delete('/saidas/:id', saidaController.deleteSaida);
 
 module.exports = router;
